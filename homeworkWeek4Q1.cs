265a00c4cc1747ff6458df572dd9b918ae20ef3e
PDeShazo1 <112003365+PDeShazo1@users.noreply.github.com>
2022-09-20 10:40:50 -0500

    Week 4 Homework question 1

---
+++ homeworkWeek4Q1.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeworkWeek4pt1;
+class Program
+{
+    static void Main(string[] args)
+    {
+// function named largest that takes 2 Integers a and b
+// and return largest one
+        static int largest(int a, int b){  
+        if(a>b){
+        return a;
+    }
+        else if(b>a){
+        return b;
+    }
+        else{
+        return -1;
+    }
+        }
+        {
+        int a = 3, b=5;
+        int lar = largest( a, b);
+        Console.WriteLine($"a = {a}; b={b}\nThe largest number is: "+lar);
+        }
+    }
+}
